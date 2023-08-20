ba0dac267c46db08febae6c72e94be19d6050abf
raybagas7 <raysam.gas@gmail.com>
2023-08-20 13:38:52 +0700

    First Init React for KaimonStore

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,33 @@
+import Catalog from '../../features/catalog/Catalog';
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from '@mui/material';
+import Header from './Header';
+import { useState } from 'react';
+import { light } from '@mui/material/styles/createPalette';
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,62 @@
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from '@mui/material';
+import { Product } from '../../app/models/product';
+
+interface Props {
+  product: Product;
+}
+
+export default function ProductCard({ product }: Props) {
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: 'secondary.main' }}>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
+        title={product.name}
+        titleTypographyProps={{
+          sx: { fontWeight: 'bold', color: 'primary.main' },
+        }}
+      />
+      <CardMedia
+        sx={{
+          height: 140,
+          backgroundSize: 'contain',
+          bgcolor: 'primary.light',
+        }}
+        image={product.pictureUrl}
+        title={product.name}
+      />
+      <CardContent>
+        <Typography gutterBottom color="sencodary" variant="h5">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {product.brand} / {product.type}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add to cart</Button>
+        <Button size="small">VIEW</Button>
+      </CardActions>
+    </Card>
+    // <ListItem key={product.id}>
+    //   <ListItemAvatar>
+    //     <Avatar src={product.pictureUrl} />
+    //   </ListItemAvatar>
+    //   <ListItemText>
+    //     {product.name} - {product.price}
+    //   </ListItemText>
+    // </ListItem>
+  );
+}
