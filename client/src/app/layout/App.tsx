56423604b557a67cface6239c81f7b9d7dc23a6c
raybagas7 <raysam.gas@gmail.com>
2024-07-06 20:27:13 +0700

    feat(ProductCard): make a card list with grid
        - use Grid from MUI andd container to ProductList and item grid to
        ProductCard
        - add Header for the web app containing the name of the apps

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Product } from '../models/products';
 import Catalog from '../../features/catalog/Catalog';
-import { Typography } from '@mui/material';
+import Header from './Header';
+import { Container, CssBaseline } from '@mui/material';
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -27,10 +28,13 @@ function App() {
   };
 
   return (
-    <div className="app">
-      <Typography variant="h1">Store</Typography>
-      <Catalog products={products} addProduct={addProduct} />
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct} />
+      </Container>
+    </>
   );
 }
 
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,4 +1,4 @@
-import { List } from '@mui/material';
+import { Grid } from '@mui/material';
 import { Product } from '../../app/models/products';
 import ProductCard from './ProductCard';
 
@@ -8,11 +8,13 @@ interface Props {
 
 const ProductList = ({ products }: Props) => {
   return (
-    <List>
+    <Grid container spacing={4}>
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <Grid key={product.id} item xs={3}>
+          <ProductCard product={product} />
+        </Grid>
       ))}
-    </List>
+    </Grid>
   );
 };
 
