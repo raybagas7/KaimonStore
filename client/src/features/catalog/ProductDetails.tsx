9096fe79d8481a1cc923853eaf06a76245a0a2c8
raybagas7 <raysam.gas@gmail.com>
2024-07-12 13:33:01 +0700

    feat(product detail): fetch the product detail
        - fetch the product detail with axios, add loading and error state

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,10 +1,31 @@
 import { Typography } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Product } from '../../app/models/products';
 
 const ProductDetails = () => {
   const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
 
-  return <Typography variant="h2">ProductDetails {id}</Typography>;
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (!product) {
+    return <h3>Product not found</h3>;
+  }
+
+  return <Typography variant="h2">{product.name}</Typography>;
 };
 
 export default ProductDetails;
