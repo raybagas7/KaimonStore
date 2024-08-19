f59d37aff64893f05f0adede4c2ee6ad5f0ead49
raybagas7 <raysam.gas@gmail.com>
2024-08-19 13:33:39 +0700

    feat(client/BasketSummary): add a summary order
        - create a summary BasketSummary component
        - use the data from basket context to calcuulate subtotal, delivery,
        and total order price

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -14,6 +14,7 @@ import { useState } from 'react';
 import agent from '../../app/api/agent';
 import { LoadingButton } from '@mui/lab';
 import { useStoreContext } from '../../app/context/StoreContext';
+import { currencyFormat } from '../../app/util/util';
 
 interface Props {
   product: Product;
@@ -58,7 +59,7 @@ const ProductCard = ({ product }: Props) => {
       />
       <CardContent>
         <Typography color="secondary" gutterBottom variant="h5">
-          ${(product.price / 100).toFixed(2)}
+          {currencyFormat(product.price)}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {product.brand} / {product.type}
